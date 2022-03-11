6da469459d3f9ef073db27ad266b091f307ba855
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-11 16:22:39 -0500

    added shift character function

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <cctype>
 #include "funcs.h"
 
 // add functions here
 
+char shiftChar2(char c, int rshift){
+
+    int alphaSize = 26;
+    int convert;
+    //uppercase letters ['A', 'Z']
+
+    if (isalpha(c) == true){
+
+        if (c >= 'A' && c <= 'Z'){
+            //convert the letter to a ascii label
+            convert = (c - int('A'));
+            c = (convert + rshift + alphaSize) % alphaSize + int('A');
+
+        }
+        //lowercase letters ['a', 'z']
+        if (c >= 'a' && c <= 'z'){
+            //convert to label
+            convert = (c - int('a'));
+            c = (convert + rshift + alphaSize) % alphaSize + int('a');
+        }
+    }
+
+    char result = char(c);
+  return result;
+}
