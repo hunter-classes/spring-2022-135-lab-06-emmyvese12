d196e59c177fab8feb272c483da70db7df4ad45b
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-11 16:24:08 -0500

    updated test-ascii for task A

--- test-ascii.cpp
+++ test-ascii.cpp
@@ -17,14 +17,17 @@ void testascii(std::string text){
 int main(){
     std::string text1, text2, text3;
     text1 = "Cat :3 Dog";
+    std::cout << "Text: " << text1 << std::endl;
     testascii(text1);
     std::cout << "\n";
 
     text2 = "Hello World!";
+    std::cout << "Text: " << text2 << std::endl;
     testascii(text2);
     std::cout << "\n";
 
     text3 = "Carrot cake is the best cake";
+    std::cout << "Text: " << text3 << std::endl;
     testascii(text3);
     
 }
