2786e84b7da3a99d23f497d2844b198b27bdcaaa
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-11 16:28:01 -0500

    added function declaration for vigenere

--- vigenere.h
+++ vigenere.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
 
-char shiftChar2(char c, int rshift);
-
 std::string encryptVigenere(std::string plaintext, std::string keyword);
 
 std::string decryptVigenere(std::string ciphertext, std::string keyword);
