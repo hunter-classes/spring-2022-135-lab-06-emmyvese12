36f73351f7b1de04fdf6185ce745e742dbca9980
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-11 16:24:23 -0500

    added test cases for task D

--- tests.cpp
+++ tests.cpp
@@ -3,6 +3,7 @@
 #include "funcs.h"
 #include "caesar.h"
 #include "vigenere.h"
+#include "decrypt.h"
 
 // add your tests here
 
@@ -26,6 +27,7 @@ TEST_CASE("Caesar Cipher Encryption"){
     CHECK(encryptCaesar("i NeEd CoFfeE!!!", 9) == "r WnNm LxOonN!!!");
 }
 
+//task C
 TEST_CASE("Vigenere Cipher Encryption using Lowercase Keys"){
     CHECK(encryptVigenere("Hello there!", "cake") == "Jevpq trite!");
     CHECK(encryptVigenere("The library closes at 7PM", "jedi") == "Clh trfuiac ftxwha jx 7SU");
@@ -37,6 +39,21 @@ TEST_CASE("Vigenere Cipher Encryption using Uppercase Keys"){
     CHECK(encryptVigenere("we will meet YoU in London", "fLOor") == "Keyword must be lowercase!");
     CHECK(encryptVigenere("the documents are ready.", "opeN") == "Keyword must be lowercase!");
 
+}
+
+//task D
+TEST_CASE("Decrypt Caesar Cipher"){
+    CHECK(decryptCaesar("Rovvy, Gybvn!", 10) == "Hello, World!");
+    CHECK(decryptCaesar("Sla'z nv av aol tvcplz!", 7) == "Let's go to the movies!");
+    CHECK(decryptCaesar("Hxd jan rwercnm cx cqn Lqarbcvjb Yjach! Bnn hxd cqnan!", 9) == "You are invited to the Christmas Party! See you there!");
+    CHECK(decryptCaesar("Wkh SDFNDJH lv zdlwlqj iru brx rq wkh vhfrqg iorru...", 3) == "The PACKAGE is waiting for you on the second floor...");
+}
+
+TEST_CASE("Decrypt Vigenere Cipher"){
+    CHECK(decryptVigenere("Jevpq, Wyvnd!", "cake") == "Hello, World!");
+    CHECK(decryptVigenere("hi uphejem uip CwMFpx lvjpq", "bubbles") == "go towards the YeLLow arrow");
+    CHECK(decryptVigenere("Pn qcwrnz ntrvh, jf wtwz ub uRtny-ce-uRplhwah.", "balloon") == "On friday night, we will go tRick-or-tReating.");
+    CHECK(decryptVigenere("Jevpq, Wyvnd!", "CaKE") == "Keyword must be lowercase!");
 }
 
 
