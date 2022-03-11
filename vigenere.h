5db77decf5d72babe0513f4ea5c1e0d08ee9674b
Emmy Veselinov <emmyveselinov@gmail.com>
2022-03-11 16:25:07 -0500

    added function declarations for task D

--- vigenere.h
+++ vigenere.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <string>
 
 char shiftChar2(char c, int rshift);
 
 std::string encryptVigenere(std::string plaintext, std::string keyword);
+
+std::string decryptVigenere(std::string ciphertext, std::string keyword);
